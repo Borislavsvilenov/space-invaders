2e6e6ad64977a0ea655999922466a090e98a58e7
Bobislav <bobo.svilenov@gmail.com>
2024-03-05 16:36:54 +0200

    added build to git ignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/borislavsvilenov/Projects/cpp games/space invaders
-# Build directory: /Users/borislavsvilenov/Projects/cpp games/space invaders/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
